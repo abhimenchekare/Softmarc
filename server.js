00cabc3987587b549be16293cb69b1227cc817a8
abhimenchekare <abhimenchekare@gmail.com>
2026-09-17 12:25:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mysql = require('mysql2/promise');
 const path = require('path');
 const multer = require('multer');
